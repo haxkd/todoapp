33b07e8deabec1b691ef7613fb17a10f79ce8dee
Abul Hasan <Abulhax@gmail.com>
2024-01-07 22:15:55 +0530

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Todolist
+
 # Create your views here.
 def index(request):
     if request.POST:
@@ -10,19 +11,17 @@ def index(request):
     context = {'todo_items':todo_items}
     return render(request,'index.html',context)
 
-
-
 def completedTodo(request, todo_id)  :  
     todo = Todolist.objects.get(pk=todo_id)
     todo.completed = True
     todo.save()
     return redirect('index')
+    
 def uncomplete(request, todo_id)  :  
     todo = Todolist.objects.get(pk=todo_id)
     todo.completed = False
     todo.save()
     return redirect('index')
-
 
 def deleteCompleted(request):
     Todolist.objects.filter(completed__exact=True).delete()
